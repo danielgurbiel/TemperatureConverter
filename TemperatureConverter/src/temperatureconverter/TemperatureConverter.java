9eb3caccd6c175aa08c0eca94701ef15be6c6dc4
daniel_gurbiel <daniel.gurbiel@gmail.com>
2018-03-11 12:53:43 +0100

    1.0

---
+++ TemperatureConverter/src/temperatureconverter/TemperatureConverter.java
@@ -0,0 +1,15 @@
+
+package temperatureconverter;
+
+/**
+ *
+ * @author Daniel Gurbiel
+ */
+
+public class TemperatureConverter {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
